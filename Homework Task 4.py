47911d1452f459fa7789e3952f163206698f279a
Avanthikacoding <aviedayilliam@gmail.com>
2024-12-01 13:02:54 +0000

    Add files via upload

---
+++ Homework Task 4.py
@@ -0,0 +1,16 @@
+a1 = 64
+a2 = 9
+a3 = 25
+a4 = 100
+a5 = 1
+squareroot1 = a1/8
+squareroot2 = a2/3
+squareroot3 = a3/5
+squareroot4 = a4/10
+squareroot5 = a5/1
+
+print("The square root of 64 is ", squareroot1)
+print("The square root of 9 is ", squareroot2)
+print("The square root of 25 is ", squareroot3)
+print("The square root of 100 is ", squareroot4)
+print("The square root of 1 is ", squareroot5)
